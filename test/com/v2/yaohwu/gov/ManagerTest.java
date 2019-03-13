aa6c1189fcab9babc2b323985c3a3cf9e9a9e42c
yaohwu <yaohwu@163.com>
2019-03-13 10:00:10 +0800

    IArgumentMatcher

--- test/com/v2/yaohwu/gov/ManagerTest.java
+++ test/com/v2/yaohwu/gov/ManagerTest.java
@@ -1,6 +1,9 @@
 package com.v2.yaohwu.gov;
 
+import com.v2.yaohwu.Company;
+import com.v2.yaohwu.Singer;
 import org.easymock.EasyMock;
+import org.easymock.IArgumentMatcher;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +14,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(PowerMockRunner.class)
@@ -46,5 +50,73 @@ public class ManagerTest {
         Whitebox.setInternalState(Manager.class, "RECORDS", expected2);
         // 获取变量 RECORDS 的值
         System.out.println(((List) Whitebox.getInternalState(Manager.class, "RECORDS")).size());
+
+        PowerMock.resetAll();
+
+        Manager.register(isMyCompany(), isMySinger());
+        EasyMock.expectLastCall().andVoid().once();
+
+        PowerMock.replayAll();
+
+        Manager.register(new Company("company-name"), new Singer("baby singer", new Date()));
+
+        PowerMock.verifyAll();
+    }
+
+    private Company isMyCompany() {
+
+        EasyMock.reportMatcher(new IArgumentMatcher() {
+            @Override
+            public boolean matches(Object argument) {
+                if (argument instanceof Company) {
+                    Company company = (Company) argument;
+                    // do something
+                    System.out.println(company.getName());
+                    System.out.println(company.toString());
+                    company.setName("new " + company.getName());
+                    System.out.println(company.getName());
+                    System.out.println(company.toString());
+
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void appendTo(StringBuffer buffer) {
+                buffer.append("company not matcher");
+            }
+        });
+        return null;
+    }
+
+    private Singer isMySinger() {
+
+        EasyMock.reportMatcher(new IArgumentMatcher() {
+            @Override
+            public boolean matches(Object argument) {
+                if (argument instanceof Singer) {
+
+                    Singer singer = (Singer) argument;
+
+                    // do something
+                    System.out.println(singer.getName());
+                    System.out.println(singer.toString());
+                    singer.setName("new " + singer.getName());
+
+                    System.out.println(singer.getName());
+                    System.out.println(singer.toString());
+
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void appendTo(StringBuffer buffer) {
+                buffer.append("singer not matcher");
+            }
+        });
+        return null;
     }
 }
